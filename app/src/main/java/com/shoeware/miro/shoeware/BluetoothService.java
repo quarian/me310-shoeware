691c82ea266f1ac34df7a21cc0d17a60fdcf7c51
Miro Nurmela <miro.nurmela@gmail.com>
2015-02-04 07:50:02 +0200

    Changing both colors

--- app/src/main/java/com/shoeware/miro/shoeware/BluetoothService.java
+++ app/src/main/java/com/shoeware/miro/shoeware/BluetoothService.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.UUID;
 
 /**
@@ -32,6 +33,7 @@ public class BluetoothService {
     private AcceptThread mAcceptThread;
     private ConnectThread mConnectThread;
     private ConnectedThread mConnectedThread;
+    private ArrayList<ConnectedThread> mConnectedThreads;
     private int mState;
 
     // Constants that indicate the current connection state
@@ -53,6 +55,11 @@ public class BluetoothService {
         mHandler.obtainMessage(MainActivity.MESSAGE_STATE_CHANGE, state, -1).sendToTarget();
     }
 
+    private void cancelConnectedThreads() {
+        for (ConnectedThread thread : mConnectedThreads)
+            thread.cancel();
+    }
+
     /**
      * Return the current connection state. */
     public synchronized int getState() {
@@ -66,7 +73,7 @@ public class BluetoothService {
         // Cancel any thread attempting to make a connection
         if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
         // Cancel any thread currently running a connection
-        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
+        //if (mConnectedThreads != null) {cancelConnectedThreads(); mConnectedThreads = null;}
         // Start the thread to listen on a BluetoothServerSocket
         if (mAcceptThread == null) {
             mAcceptThread = new AcceptThread();
@@ -85,7 +92,6 @@ public class BluetoothService {
             if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
         }
         // Cancel any thread currently running a connection
-        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
         // Start the thread to connect with the given device
         mConnectThread = new ConnectThread(device);
         mConnectThread.start();
@@ -100,13 +106,14 @@ public class BluetoothService {
         if (D) Log.d(TAG, "connected");
         // Cancel the thread that completed the connection
         if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
-        // Cancel any thread currently running a connection
-        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
         // Cancel the accept thread because we only want to connect to one device
-        if (mAcceptThread != null) {mAcceptThread.cancel(); mAcceptThread = null;}
+        //if (mAcceptThread != null) {mAcceptThread.cancel(); mAcceptThread = null;}
         // Start the thread to manage the connection and perform transmissions
-        mConnectedThread = new ConnectedThread(socket);
-        mConnectedThread.start();
+        ConnectedThread thread = new ConnectedThread(socket);
+        thread.start();
+        if (mConnectedThreads == null)
+            mConnectedThreads = new ArrayList<ConnectedThread>();
+        mConnectedThreads.add(thread);
         // Send the name of the connected device back to the UI Activity
         Message msg = mHandler.obtainMessage(MainActivity.MESSAGE_DEVICE_NAME);
         Bundle bundle = new Bundle();
@@ -121,7 +128,7 @@ public class BluetoothService {
     public synchronized void stop() {
         if (D) Log.d(TAG, "stop");
         if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
-        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
+        if (mConnectedThreads != null) {cancelConnectedThreads(); mConnectedThreads = null;}
         if (mAcceptThread != null) {mAcceptThread.cancel(); mAcceptThread = null;}
         setState(STATE_NONE);
     }
@@ -132,14 +139,15 @@ public class BluetoothService {
      */
     public void write(byte[] out) {
         // Create temporary object
-        ConnectedThread r;
+        ArrayList<ConnectedThread> r;
         // Synchronize a copy of the ConnectedThread
         synchronized (this) {
             if (mState != STATE_CONNECTED) return;
-            r = mConnectedThread;
+            r = mConnectedThreads;
         }
         // Perform the write unsynchronized
-        r.write(out);
+        for (ConnectedThread thread : r)
+            thread.write(out);
     }
     /**
      * Indicate that the connection attempt failed and notify the UI Activity.
